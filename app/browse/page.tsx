83390990e551227adbd7dc97e6123866d8e77fd1
Jettaroni10 <masterjett16@gmail.com>
2026-01-30 22:21:26 -0600

    Force dynamic browse for signed URLs

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,5 +1,9 @@
 import BrowseView from "@/components/BrowseView";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 type PageProps = {
   searchParams?: Record<string, string | string[] | undefined>;
 };

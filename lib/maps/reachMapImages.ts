b337e42d7fe35aadb00f1b8072fe867128a8e747
Jettaroni10 <masterjett16@gmail.com>
2026-02-09 05:00:29 -0600

    Self-host Reach map images

--- lib/maps/reachMapImages.ts
+++ lib/maps/reachMapImages.ts
@@ -4,35 +4,35 @@ const normalizeMapName = (value?: string | null) => {
 };
 
 export const REACH_MAP_IMAGE_URL: Record<string, string> = {
-  "Boardwalk": "https://www.halopedia.org/File:HaloReach_-_Boardwalk.jpg",
-  "Boneyard": "https://www.halopedia.org/File:HaloReach_-_Boneyard.jpg",
-  "Countdown": "https://www.halopedia.org/File:HaloReach_-_Countdown.jpg",
-  "Powerhouse": "https://www.halopedia.org/File:HaloReach_-_Powerhouse.jpg",
-  "Reflection": "https://www.halopedia.org/File:HaloReach_-_Reflection.jpg",
-  "Spire": "https://www.halopedia.org/File:HaloReach_-_Spire.jpg",
-  "Sword Base": "https://www.halopedia.org/File:HaloReach_-_Sword_Base.jpg",
-  "Zealot": "https://www.halopedia.org/File:HaloReach_-_Zealot.jpg",
-  "Forge World": "https://www.halopedia.org/File:HaloReach_-_Forge_World.jpg",
-  "Asylum": "https://www.halopedia.org/File:HaloReach_-_Asylum.jpg",
-  "Hemorrhage": "https://www.halopedia.org/File:HaloReach_-_Hemorrhage.jpg",
-  "Paradiso": "https://www.halopedia.org/File:HaloReach_-_Paradiso.jpg",
-  "Pinnacle": "https://www.halopedia.org/File:HaloReach_-_Pinnacle.jpg",
-  "The Cage": "https://www.halopedia.org/File:HaloReach_-_The_Cage.jpg",
-  "Anchor 9": "https://www.halopedia.org/File:HaloReach_-_Anchor_9.jpg",
-  "Breakpoint": "https://www.halopedia.org/File:HaloReach_-_Breakpoint.jpg",
-  "Tempest": "https://www.halopedia.org/File:HaloReach_-_Tempest.jpg",
-  "Condemned": "https://www.halopedia.org/File:HaloReach_-_Condemned.jpg",
-  "Highlands": "https://www.halopedia.org/File:HaloReach_-_Highlands.jpg",
-  "Battle Canyon": "https://www.halopedia.org/File:HaloReach_-_Battle_Canyon.jpg",
-  "Breakneck": "https://www.halopedia.org/File:HaloReach_-_Breakneck.jpg",
-  "High Noon": "https://www.halopedia.org/File:HaloReach_-_High_Noon.jpg",
-  "Penance": "https://www.halopedia.org/File:HaloReach_-_Penance.jpg",
-  "Ridgeline": "https://www.halopedia.org/File:HaloReach_-_Ridgeline.jpg",
-  "Solitary": "https://www.halopedia.org/File:HaloReach_-_Solitary.jpg",
+  "Boardwalk": "/maps/reach/boardwalk.jpg",
+  "Boneyard": "/maps/reach/boneyard.jpg",
+  "Countdown": "/maps/reach/countdown.jpg",
+  "Powerhouse": "/maps/reach/powerhouse.jpg",
+  "Reflection": "/maps/reach/reflection.jpg",
+  "Spire": "/maps/reach/spire.jpg",
+  "Sword Base": "/maps/reach/sword-base.jpg",
+  "Zealot": "/maps/reach/zealot.jpg",
+  "Forge World": "/maps/reach/forge-world.png",
+  "Asylum": "/maps/reach/asylum.jpg",
+  "Hemorrhage": "/maps/reach/hemorrhage.png",
+  "Paradiso": "/maps/reach/paradiso.jpg",
+  "Pinnacle": "/maps/reach/pinnacle.jpg",
+  "The Cage": "/maps/reach/the-cage.jpg",
+  "Anchor 9": "/maps/reach/anchor-9.png",
+  "Breakpoint": "/maps/reach/breakpoint.png",
+  "Tempest": "/maps/reach/tempest.png",
+  "Condemned": "/maps/reach/condemned.png",
+  "Highlands": "/maps/reach/highlands.png",
+  "Battle Canyon": "/maps/reach/battle-canyon.png",
+  "Breakneck": "/maps/reach/breakneck.png",
+  "High Noon": "/maps/reach/high-noon.png",
+  "Penance": "/maps/reach/penance.png",
+  "Ridgeline": "/maps/reach/ridgeline.png",
+  "Solitary": "/maps/reach/solitary.png",
 };
 
 export const REACH_DEFAULT_BG =
-  "https://www.halopedia.org/File:HaloReach_-_Multiplayer.jpg";
+  "/maps/reach/halo-reach-multiplayer.jpg";
 
 const REACH_MAP_IMAGE_URL_NORMALIZED = Object.entries(REACH_MAP_IMAGE_URL).reduce<
   Record<string, string>
